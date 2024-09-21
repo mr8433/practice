273a4280b9926746152b00236e5d5337cbffa205
Mayank Rajput <130533182+mr8433@users.noreply.github.com>
2024-09-21 10:27:06 +0530

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,15 @@
+s1={2,4,6,8,10}
+s2={1,3,5,7,9}
+# s1.clear()
+# s2.add(2)
+# s1.(10)
+# s3=s1.union(s2)
+# s1.intersection_update(s2)
+# print(s1.isdisjoint(s2))
+# print(s1.difference(s2))
+print(s2)
+print(s1)
+
+
+
+
